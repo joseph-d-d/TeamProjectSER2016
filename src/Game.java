b4082de1933003dfc33b7661df13d332d3fc11b8
SBTheTurtle <janice@janicewallace.net>
2016-02-29 16:03:11 -0800

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -11,26 +11,24 @@ public class Game extends Canvas {
 		JFrame frame = new JFrame("Pacman");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Renderer renderer = new  Renderer(frame, gameWindow);
-		Pacman pacman = new Pacman(2, 2, Color.YELLOW, input, gameBoard);
+		Pacman pacman = new Pacman(2, 2, Color.YELLOW, input, gameBoard, true);
 		Ghost[] ghosts = new Ghost[4];
-		ghosts[0] = new Ghost(17, 15, Color.PINK, input, gameBoard, 
-				Behavior.RANDOM, Direction.LEFT);
-		ghosts[1] = new Ghost(16, 15, Color.ORANGE, input, gameBoard, 
-				Behavior.RANDOM, Direction.RIGHT);
-		ghosts[2] = new Ghost(14, 15, Color.GREEN, input, gameBoard, 
-				Behavior.FOLLOW_LEFT, Direction.UP);
-		ghosts[3] = new Ghost(15, 15, Color.RED, input, gameBoard, 
-				Behavior.FOLLOW_RIGHT, Direction.UP);
+		ghosts[0] = new Ghost(17, 15, Color.PINK, input, gameBoard, true, Behavior.AGGRESSIVE);
+		ghosts[1] = new Ghost(16, 15, Color.ORANGE, input, gameBoard, true, Behavior.RANDOM);
+		ghosts[2] = new Ghost(14, 15, Color.GREEN, input, gameBoard, true, Behavior.FOLLOW_LEFT);
+		ghosts[3] = new Ghost(15, 15, Color.RED, input, gameBoard, true, Behavior.FOLLOW_RIGHT);
 
 		//Game loop
 		while (!exitGame) {
 			renderer.render(gameBoard, pacman, ghosts);
-			pacman.update();
+			
 			for(Ghost ghost : ghosts){
 				ghost.update();
 			}
+			pacman.update();
 			input.update();
 			Thread.yield();
 		}
+		
 	} // end main
 } // end class
